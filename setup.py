af23af242e704330d863ca6e6b1638dcd4f19e33
JankyChris <chrissi@janky.de>
2022-11-10 11:30:16 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My submission to the Kaggle House Prices challenge',
+    author='Christoph Jankowsky',
+    license='MIT',
+)
